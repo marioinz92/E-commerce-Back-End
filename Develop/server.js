e62ac5185cfea59049a731b0a725adb7d5aa0711
Mario Inzunza <marioelgallo92@gmail.com>
2023-09-05 09:42:37 -0700

    import sequelize add pass

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./sequelize'); // Import your Sequelize configuration
 // import sequelize connection
 
 const app = express();
@@ -10,7 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+// Sync Sequelize models to the database
+sequelize.sync()
+  .then(() => {
+    console.log('Sequelize models synced to the database');
+
+    // sync sequelize models to the database, then turn on the server
+    // Start the Express.js server
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error syncing Sequelize models:', error);
+  });
+    
+    
+    
