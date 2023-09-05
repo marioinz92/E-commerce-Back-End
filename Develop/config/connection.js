07f7a0724cb9dfaea623af4bcec16de9582c46be
Mario Inzunza <marioelgallo92@gmail.com>
2023-09-05 09:51:22 -0700

    fix connection defined columns

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,6 +1,8 @@
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 const Sequelize = require('sequelize');
+
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
